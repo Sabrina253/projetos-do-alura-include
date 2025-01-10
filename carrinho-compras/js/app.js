d0cf9d1090f7cb341ef62f64b6208c9484addf5a
sabrina253 <smma1@aluno.ifal.edu.br>
2025-01-10 17:35:39 -0300

    :recycle: refactor: atualizei app

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,12 +1,18 @@
 let totalGeral;
-limpar();
+limparTela();
 
-function adicionar() {
-    let produto = document.getElementById('produto').value;
+function adicionarProduto() {
+    let produto = document.getElementById('in-produto').value;
+    let erro = document.querySelector('#out-erro');
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade').value;
+    let quantidade = document.getElementById('in-quantidade').value;
     let preco = quantidade * valorUnitario;
+
+    if (quantidade == ''){
+        alert('Informe um valor válido!');
+        return;
+    }
 
     let carrinho = document.getElementById('lista-produtos');
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
@@ -16,11 +22,12 @@ function adicionar() {
     totalGeral = totalGeral + preco;
     let campoTotal = document.getElementById('valor-total');
     campoTotal.textContent = `R$ ${totalGeral}`;
-    document.getElementById('quantidade').value = 0;
+    document.getElementById('in-quantidade').value = '';
+    document.getElementById('in-quantidade').focus();
 }
 
-function limpar() {
+function limparTela() {
     totalGeral = 0;
     document.getElementById('lista-produtos').innerHTML = '';
-    document.getElementById('valor-total').textContent = 'R$0';
+    document.getElementById('valor-total').textContent = '';
 }
